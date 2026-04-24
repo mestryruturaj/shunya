3aafb0f7f86523153079f1e20948ca46fb8b10f6
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-04-24 20:58:13 +0530

    Update Player | PUT | Added user repo method for validation

--- src/main/java/io/two/bit/saint/shunya/dao/UserRepository.java
+++ src/main/java/io/two/bit/saint/shunya/dao/UserRepository.java
@@ -3,10 +3,14 @@ package io.two.bit.saint.shunya.dao;
 import io.two.bit.saint.shunya.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     public Optional<User> findByEmail(String email);
 
     public Optional<User> findByMobileNumber(String mobileNumber);
+
+    Optional<List<User>> findByUserIdOrEmailOrMobileNumber(Long userId, String email, String mobileNumber);
+
 }
