f19f8090e5a5c0c20e9371b6d44ec9c056e7f868
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-05-15 15:40:29 +0530

    /season | POST | create season | Defined entity, enums for validation purpose, accepted values in application yaml

--- src/main/java/io/two/bit/saint/shunya/entity/Season.java
+++ src/main/java/io/two/bit/saint/shunya/entity/Season.java
@@ -3,12 +3,14 @@ package io.two.bit.saint.shunya.entity;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class Season extends AbstractAuditEntity {
