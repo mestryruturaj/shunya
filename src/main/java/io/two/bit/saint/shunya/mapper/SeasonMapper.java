9dae6470b109018c97193f5a10100c8465a25e40
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-07-19 22:09:29 +0530

    /season/{season-id} | PUT | update season | added api spec

--- src/main/java/io/two/bit/saint/shunya/mapper/SeasonMapper.java
+++ src/main/java/io/two/bit/saint/shunya/mapper/SeasonMapper.java
@@ -4,15 +4,16 @@ import io.two.bit.saint.shunya.entity.Season;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-import org.openapitools.model.SeasonCreateRequest;
+import org.openapitools.model.SeasonBase;
 import org.openapitools.model.SeasonResponse;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface SeasonMapper {
-    public Season mapToSeasonEntityFromSeasonCreateRequest(SeasonCreateRequest seasonCreateRequest);
+    public Season mapToSeasonEntityFromSeasonBase(SeasonBase seasonBase);
 
     @Mapping(target = "tournamentId", source = "tournament.id")
+    @Mapping(target = "seasonId", source = "id")
     public SeasonResponse mapToSeasonResponseFromSeasonEntity(Season season);
 }
