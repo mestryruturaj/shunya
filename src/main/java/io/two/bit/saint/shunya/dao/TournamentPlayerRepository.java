11457d6a449cc90d7d914ca21580f5afe5a866d5
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-07-24 23:12:32 +0530

    /tournament-player/tournament/{tournament-id} | GET | get players enrolled for the tournament | added query to fetch joined player list

--- src/main/java/io/two/bit/saint/shunya/dao/TournamentPlayerRepository.java
+++ src/main/java/io/two/bit/saint/shunya/dao/TournamentPlayerRepository.java
@@ -1,10 +1,17 @@
 package io.two.bit.saint.shunya.dao;
 
+import io.two.bit.saint.shunya.entity.Player;
 import io.two.bit.saint.shunya.entity.TournamentPlayer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TournamentPlayerRepository extends JpaRepository<TournamentPlayer, Long> {
     boolean existsByTournamentIdAndPlayerId(Long tournamentId, Long playerId);
+
+    @Query("SELECT tp.player FROM TournamentPlayer tp JOIN tp.player p WHERE tp.tournament.id = :tournamentId")
+    List<Player> findPlayersByTournamentId(@Param("tournamentId") Long tournamentId);
 }
