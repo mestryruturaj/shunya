a2584b525eef6c1509c4511bd5e9b66872355091
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-07-29 09:38:30 +0530

    /season-player | POST | impl | added SeasonPlayer delegate layer implementation

---
+++ src/main/java/io/two/bit/saint/shunya/delegate/SeasonPlayerManagementApiDelegateImpl.java
@@ -0,0 +1,21 @@
+package io.two.bit.saint.shunya.delegate;
+
+import io.two.bit.saint.shunya.service.SeasonPlayerManagementService;
+import lombok.RequiredArgsConstructor;
+import org.openapitools.api.SeasonPlayerManagementApiDelegate;
+import org.openapitools.model.SeasonPlayerCreateRequest;
+import org.openapitools.model.SeasonPlayerResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SeasonPlayerManagementApiDelegateImpl implements SeasonPlayerManagementApiDelegate {
+    private final SeasonPlayerManagementService seasonPlayerManagementService;
+
+    @Override
+    public ResponseEntity<SeasonPlayerResponse> createSeasonPlayer(SeasonPlayerCreateRequest seasonPlayerCreateRequest) {
+        return new ResponseEntity<>(seasonPlayerManagementService.createSeasonPlayer(seasonPlayerCreateRequest), HttpStatus.CREATED);
+    }
+}
