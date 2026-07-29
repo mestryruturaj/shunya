ee7c50f4109738d00155221e467ff635b93ea729
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-07-29 22:07:43 +0530

    /season-player/season/{season-id}/player | GET | impl | added business logic

--- src/main/java/io/two/bit/saint/shunya/service/SeasonPlayerManagementServiceImpl.java
+++ src/main/java/io/two/bit/saint/shunya/service/SeasonPlayerManagementServiceImpl.java
@@ -3,6 +3,8 @@ package io.two.bit.saint.shunya.service;
 import io.two.bit.saint.shunya.dao.SeasonPlayerRepository;
 import io.two.bit.saint.shunya.dto.SeasonPlayerValidContext;
 import io.two.bit.saint.shunya.dto.enums.RequestType;
+import io.two.bit.saint.shunya.entity.Player;
+import io.two.bit.saint.shunya.entity.Season;
 import io.two.bit.saint.shunya.entity.SeasonPlayer;
 import io.two.bit.saint.shunya.exception.InvalidArgumentException;
 import io.two.bit.saint.shunya.mapper.PlayerMapper;
@@ -11,7 +13,10 @@ import io.two.bit.saint.shunya.validator.SeasonPlayerValidator;
 import lombok.RequiredArgsConstructor;
 import org.openapitools.model.SeasonPlayerCreateRequest;
 import org.openapitools.model.SeasonPlayerResponse;
+import org.openapitools.model.SeasonPlayersResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +25,7 @@ public class SeasonPlayerManagementServiceImpl implements SeasonPlayerManagement
     private final SeasonPlayerRepository seasonPlayerRepository;
     private final SeasonMapper seasonMapper;
     private final PlayerMapper playerMapper;
+    private final SeasonManagementService seasonManagementService;
 
     @Override
     public SeasonPlayerResponse createSeasonPlayer(SeasonPlayerCreateRequest seasonPlayerCreateRequest) {
@@ -44,11 +50,28 @@ public class SeasonPlayerManagementServiceImpl implements SeasonPlayerManagement
 
     @Override
     public SeasonPlayerResponse getSeasonPlayerById(Long seasonPlayerId) {
-        seasonPlayerValidator.validateIdField(seasonPlayerId, "Season Player");
+        seasonPlayerValidator.validateIdField(seasonPlayerId, SeasonPlayer.class.getSimpleName());
 
         SeasonPlayer fetchedSeasonPlayer = seasonPlayerRepository.findById(seasonPlayerId)
                 .orElseThrow(() -> new InvalidArgumentException("Season player not found with id: " + seasonPlayerId));
 
         return buildSeasonPlayerResponse(fetchedSeasonPlayer);
+    }
+
+    @Override
+    public SeasonPlayersResponse getSeasonPlayersBySeasonId(Long seasonId) {
+        Season fetchedSeason = seasonManagementService.fetchById(seasonId);
+        List<Player> fetchedPlayersBySeasonId = seasonPlayerRepository.findPlayersBySeasonId(seasonId);
+
+        return buildSeasonPlayersResponse(fetchedSeason, fetchedPlayersBySeasonId);
+    }
+
+    private SeasonPlayersResponse buildSeasonPlayersResponse(Season season, List<Player> players) {
+        SeasonPlayersResponse seasonPlayersResponse = new SeasonPlayersResponse();
+        seasonPlayersResponse.setSeason(seasonMapper.mapToSeasonInfoFromSeasonEntity(season));
+        seasonPlayersResponse.setPlayers(players.stream()
+                .map(playerMapper::mapToPlayerInfoFromPlayerEntity)
+                .toList());
+        return seasonPlayersResponse;
     }
 }
