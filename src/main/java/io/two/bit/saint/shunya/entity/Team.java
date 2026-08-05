390e9cc168d2e68e13726147c528e9c325de5927
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-08-05 22:23:11 +0530

    POST /team/create-team | impl | added team entity

---
+++ src/main/java/io/two/bit/saint/shunya/entity/Team.java
@@ -0,0 +1,24 @@
+package io.two.bit.saint.shunya.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "teams")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String teamName;
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="captain_id", referencedColumnName = "id")
+    private Player captain;
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="vice_captain_id", referencedColumnName = "id")
+    private Player viceCaptain;
+}
