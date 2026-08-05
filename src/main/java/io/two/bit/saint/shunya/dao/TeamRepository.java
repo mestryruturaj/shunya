787ac32ec8f03ea7970ddd632f6bbe1eb4f20b03
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-08-05 22:23:24 +0530

    POST /team/create-team | impl | added team repository

---
+++ src/main/java/io/two/bit/saint/shunya/dao/TeamRepository.java
@@ -0,0 +1,7 @@
+package io.two.bit.saint.shunya.dao;
+
+import io.two.bit.saint.shunya.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
