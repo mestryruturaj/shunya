2b7b71771eb8dde0aba9803b5c40d07450ba7977
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-08-06 22:12:11 +0530

    GET /team | impl | added business logic

--- src/main/java/io/two/bit/saint/shunya/service/TeamManagementService.java
+++ src/main/java/io/two/bit/saint/shunya/service/TeamManagementService.java
@@ -3,9 +3,13 @@ package io.two.bit.saint.shunya.service;
 import org.openapitools.model.TeamCreateRequest;
 import org.openapitools.model.TeamResponse;
 
+import java.util.List;
+
 public interface TeamManagementService {
 
     TeamResponse createTeam(TeamCreateRequest teamCreateRequest);
 
     TeamResponse getTeamById(Long teamId);
+
+    List<TeamResponse> getAllTeams();
 }
