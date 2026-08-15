485313de1fd63397905fe908a013998676d45cae
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-08-15 15:30:00 +0530

    POST /organize | impl | added delegate layer

---
+++ src/main/java/io/two/bit/saint/shunya/delegate/OrganizerManagementApiDelegateImpl.java
@@ -0,0 +1,21 @@
+package io.two.bit.saint.shunya.delegate;
+
+import io.two.bit.saint.shunya.service.OrganizerManagementService;
+import lombok.RequiredArgsConstructor;
+import org.openapitools.api.OrganizerManagementApiDelegate;
+import org.openapitools.model.OrganizerCreateRequest;
+import org.openapitools.model.OrganizerResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OrganizerManagementApiDelegateImpl implements OrganizerManagementApiDelegate {
+    private final OrganizerManagementService organizerManagementService;
+
+    @Override
+    public ResponseEntity<OrganizerResponse> createOrganizer(OrganizerCreateRequest organizerCreateRequest) {
+        return new ResponseEntity<>(organizerManagementService.createOrganizer(organizerCreateRequest), HttpStatus.CREATED);
+    }
+}
