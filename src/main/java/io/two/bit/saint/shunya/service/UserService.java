e2e250dc1b8a3b0df4c00e5e6e7fde7cdb2f93f9
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-08-15 15:29:15 +0530

    POST /organize | impl | enhance user service to support Organizer Management

--- src/main/java/io/two/bit/saint/shunya/service/UserService.java
+++ src/main/java/io/two/bit/saint/shunya/service/UserService.java
@@ -1,5 +1,6 @@
 package io.two.bit.saint.shunya.service;
 
+import io.two.bit.saint.shunya.entity.User;
 import org.openapitools.model.LoginRequest;
 import org.openapitools.model.LoginResponse;
 import org.openapitools.model.SignupRequest;
@@ -9,4 +10,6 @@ public interface UserService {
     public UserDto signup(SignupRequest signupRequest);
 
     public LoginResponse login(LoginRequest loginRequest);
+
+    User fetchbyId(Long id);
 }
