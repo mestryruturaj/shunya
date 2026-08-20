c9aec4315d7ec2e9956fef73f91144275fdf2904
Ruturaj Mestry <mestryruturaj@gmail.com>
2026-08-20 21:21:12 +0530

    POST /auction/{auction-id}/organizer | impl | added dao for AuctionOrganizer

---
+++ src/main/java/io/two/bit/saint/shunya/dao/AuctionOrganizerRepository.java
@@ -0,0 +1,17 @@
+package io.two.bit.saint.shunya.dao;
+
+import io.two.bit.saint.shunya.entity.AuctionOrganizer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AuctionOrganizerRepository extends JpaRepository<AuctionOrganizer, Long> {
+
+    @Query("SELECT ao FROM AuctionOrganizer ao "
+            + "JOIN FETCH ao.auction "
+            + "JOIN FETCH ao.organizer "
+            + "WHERE ao.auction.id = :auctionId")
+    List<AuctionOrganizer> findAllByAuctionId(@Param("auctionId") Long auctionId);
+}
